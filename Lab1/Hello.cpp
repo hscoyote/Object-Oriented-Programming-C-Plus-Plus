d6e653b6bd970384ee743e1da118150b06808cc0
hscoyote <157756396+hscoyote@users.noreply.github.com>
2024-01-25 15:25:09 -0800

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {A short program to print two lines of a hello message}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
