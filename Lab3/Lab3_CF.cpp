15832822e257079668905df13a90850e5a9e1e40
hscoyote <157756396+hscoyote@users.noreply.github.com>
2024-02-19 18:16:53 -0800

    Add files via upload

---
+++ Lab3/Lab3_CF.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main (){
+    int x = 0;
+    int y = 5;
+
+    while (x < y){
+        printf("%i\n", x);
+        x = x + 3;
+        
+        if(x % 2 == 0){
+            printf("x is even\n");
+        }
+        else{
+            printf("x is odd\n");
+        }
+    }
+}
