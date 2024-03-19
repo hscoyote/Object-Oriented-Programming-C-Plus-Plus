d85d4565a21f7036702697194bfaa197560e3ff5
hscoyote <157756396+hscoyote@users.noreply.github.com>
2024-03-19 13:51:36 -0700

    Update RPG.cpp

--- Lab 5/RPG.cpp
+++ Lab 5/RPG.cpp
@@ -1,4 +1,6 @@
 #include "RPG.h"
+#include <iostream>
+
 
 RPG::RPG()
 {
@@ -9,7 +11,7 @@ RPG::RPG()
     type = "warrior";
     skills[0] = "slash";
     skills[1] = "parry"; 
-};
+}
 
 RPG::RPG(string name, int health, int strength, int defense, string type)
 {
@@ -95,3 +97,53 @@ bool RPG::isAlive() const {
    bool isAlive = health > 0;
    return isAlive; 
 }
+
+//attack decreases the opponents health by (strength- opponents defense)
+
+void RPG::attack(RPG * opponent){
+    int opp_health = (*opponent).getHealth();
+    int opp_def = (*opponent).getDefense();
+
+    if (strength - opp_def > 0) {
+        int new_health =opp_health - (strength - opp_def);
+        (*opponent).updateHealth(new_health);
+    } else {
+        int new_health = opp_health - 0;
+        (*opponent).updateHealth(new_health);
+    }
+
+
+// old code for ^^^^
+//     if ((strength - (*opponent).defense) > 0 ){
+//         (*opponent).health = (*opponent).health - (strength - (*opponent).defense);
+//     } else {
+//         (*opponent).health = (*opponent).health - 0;
+//     }
+
+//     (*opponent).updateHealth((*opponent).health);
+// }
+
+}
+
+void RPG::useSkill(RPG * opponent){
+    for(int i=0; i < SKILL_SIZE; i++){
+        printf("Skill %i: %s\n", i, skills[i].c_str());
+    }
+
+    //int created
+    int chosen_skill_index;
+
+    //prompts the user to choose their skill
+    printf("Choose a skill to use: Enter 0 or 1\n");
+
+    //takes the user input for chosen_skill_index
+    cin >> chosen_skill_index;
+
+    //chosen skill assigned to a string
+    string chosen_skill = skills[chosen_skill_index];
+
+    printAction(chosen_skill, *opponent);
+    attack(opponent);
+
+}
+
