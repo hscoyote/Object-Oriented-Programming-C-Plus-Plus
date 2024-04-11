46b4fe4968cab96f5c2f6aadda0d928d628428bf
hscoyote <157756396+hscoyote@users.noreply.github.com>
2024-04-10 23:30:56 -0700

    Add files via upload

---
+++ Lab 8/vector.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/**
+ * @brief prints the elements in the array and their memory locations
+ * 
+ * @param v - vector of integers
+*/
+// void printMemVec(...){
+
+// }
+
+
+/**
+ * @brief increments all of the elements in v by 10
+ * 
+ * @param v - address to a vector of integers 
+*/
+void incVecBy10(...){
+
+}
+
+
+int main() {
+    //create a constant integer called SIZE that is of value 5
+
+    //create a vector integers caled vec that can hold up to 5 elements 
+
+    //use a for loop to populate vec with the values 100 to 104
+
+    printf("Before Increment---------------\n");
+    //call printMemVec(...) on vec 
+
+    //call incBy10(...) on vec 
+
+
+    printf("After Increment------------------\n");
+    //call printMemVec(...) on vec again to view the changes
+
+
+    //remove last element of the vec
+    //(refer to docs https://cplusplus.com/reference/vector/vector/#google_vignette)
+
+    printf("After Pop-------------------\n");
+    //call printMemVec(...) on vec again to view the changes
+
+    //append 101 and 102 at the end of vec
+    // (refer to docs https://cplusplus.com/reference/vector/vector/#google_vignette)
+
+    printf("After Push---------------\n");
+    //call printMemVec(...) on vec again to view the changes 
+
+    return 0;
+}
